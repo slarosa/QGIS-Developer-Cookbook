4ffb78dba198eb8ca807d1c9321b371b28ae2887
p1d1d1 <pasquale.didonato@gmail.com>
2012-03-14 08:58:52 +0100

    Update italian_it/source/conf.py

--- italian_it/source/conf.py
+++ italian_it/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyQGIS Developer Cookbook'
-copyright = u'2010-2011, Martin Dobias - tradotto da Pasquale Di Donato'
+copyright = u'2010-2011, Martin Dobias'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
